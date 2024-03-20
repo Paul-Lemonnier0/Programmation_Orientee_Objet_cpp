4a64024c8051dbc00f9fa33e3482bb34eef93102
Paul49070 <paul.lemonnier49070@gmail.com>
2024-03-20 14:50:40 +0100

    Premier commit

---
+++ TP8_1/calcul.cpp
@@ -0,0 +1,1 @@
+#include "calcul.h"
---
+++ TP8_1/main.cpp
@@ -0,0 +1,9 @@
+#include <QtWidgets>
+#include "calcul.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    calcul c;
+    return app.exec();
+}
